6d2848bee24c424ab1e08e666dac65124457fdfc
Ryan Porter <Ryan.Porter@trojans.dsu.edu>
2021-04-01 20:25:01 -0500

    Finished Expert difficulty

--- MineSweeper/MineSweeper/Difficulty/Expert.cs
+++ MineSweeper/MineSweeper/Difficulty/Expert.cs
@@ -8,5 +8,26 @@ namespace MineSweeper.Difficulty
 {
     class Expert : IDifficulty
     {
+        public int Height
+        {
+            get
+            {
+                return 16;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return 30;
+            }
+        }
+        public int NumMines
+        {
+            get
+            {
+                return 99;
+            }
+        }
     }
 }
