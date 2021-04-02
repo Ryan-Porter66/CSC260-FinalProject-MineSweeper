a7b73da4907fa3abfabf7422c2ba0834ddcc486e
Ryan Porter <Ryan.Porter@trojans.dsu.edu>
2021-04-01 20:24:14 -0500

    Finished Intermediate difficulty.

--- MineSweeper/MineSweeper/Difficulty/Intermediate.cs
+++ MineSweeper/MineSweeper/Difficulty/Intermediate.cs
@@ -8,5 +8,27 @@ namespace MineSweeper.Difficulty
 {
     class Intermediate : IDifficulty
     {
+        //values gotten from https://minesweeperonline.com/
+        public int Height
+        {
+            get
+            {
+                return 16;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return 16;
+            }
+        }
+        public int NumMines
+        {
+            get
+            {
+                return 40;
+            }
+        }
     }
 }
